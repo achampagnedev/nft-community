413b5fc69e2f54ca587ed9a45cbce8af45894d35
Alexandre Champagne <achampagnedev@gmail.com>
2022-07-10 20:45:20 +0800

    querying one pic

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["img.x2y2.io"]
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,42 @@
 import {ethers} from "ethers";
-import {useState} from "react";
 import Image from "next/image";
+import {useEffect, useState} from "react";
 
 const BOKI_CONTRACT = "0x248139aFB8d3A2e16154FbE4Fb528A3a214fd8E7";
+const BOKI_TOKEN_ID = 4715;
 
-export default function Home({title, description}) {
-  const [community, setCommunity] = useState("____")
-  const [bokiImg, setBokiImg] = useState("")
+export default function Home() {
+  const [token, setToken] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+  const contractAbi = [
+    "function name() view returns (string)",
+    "function tokenURI(uint256 _tokenId) view returns (string)"
+  ];
 
-  const handleClick = async () => {
+  useEffect(async () => {
+    setLoading(true)
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
-    const contractAbi = [
-      "function name() view returns (string)",
-      "function tokenURI(uint256 _tokenId) view returns (string)"
-    ];
+    await provider.send("eth_requestAccounts", {});
     const contractInstance = new ethers.Contract(BOKI_CONTRACT, contractAbi, provider)
-    const name = await contractInstance.name()
-    setCommunity(name);
-    const asevenexBokiURI = await contractInstance.tokenURI(4715);
-    const asevenexBokiData = await fetch(asevenexBokiURI).then(result => console.log(result));
-    // console.log(asevenexBoki);
-    setBokiImg(asevenexBoki)
-  }
+    const tokenURI = await contractInstance.tokenURI(BOKI_TOKEN_ID);
+
+    fetch(tokenURI)
+      .then((result) => result.json())
+      .then(data => {
+        console.log(data);
+        setToken(data)
+        setLoading(false)
+      });
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!token) return <p>No token data</p>
 
   return (
     <div>
-      <h1>NFT Community for {community}</h1>
+      <h1>NFT Community for Boki</h1>
       <p>"This is a showcase app for NFT communities"</p>
-      {bokiImg && <Image src={bokiImg} width={400} heigth={400} layout="fixed" />}
-      <button onClick={handleClick}>Connect Wallet</button>
+      <Image src={`https://img.x2y2.io/v2/1/${BOKI_CONTRACT}/${BOKI_TOKEN_ID}/1440/image.jpg`} width={400} height={400} alt={token.name} />
     </div>
   )
 }
@@ -38,7 +45,6 @@ export async function getStaticProps() {
 
   return {
     props:{
-
     }
   }
 }
